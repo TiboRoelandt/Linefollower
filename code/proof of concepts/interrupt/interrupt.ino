6c88f990088fa0a6284c1779377363bffe3d86ac
TiboRoelandt <146442904+TiboRoelandt@users.noreply.github.com>
2023-11-17 13:54:16 +0100

    interrupt.ino

--- code/proof of concepts/interrupt/interrupt.ino
+++ code/proof of concepts/interrupt/interrupt.ino
@@ -1,30 +1,24 @@
-#include <ezButton.h>
-int LED = 3;
-
-#define LOOP_STATE_STOPPED 0
-#define LOOP_STATE_STARTED 1
-
-ezButton button(2);  // create ezButton object that attach to pin 7;
-int loopState = LOOP_STATE_STOPPED;
-
+const int LED = 3;
+const int BUTTON = 2;
+bool running;
 void setup() {
-pinMode(LED, OUTPUT);
-
-  button.setDebounceTime(50); // set debounce time to 50 milliseconds
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON), blinkLed, RISING);
+  Serial.begin(9600);
 }
-
 void loop() {
-  button.loop(); // MUST call the loop() function first
-
-  if (button.isPressed()) {
-    if (loopState == LOOP_STATE_STOPPED)
-      loopState = LOOP_STATE_STARTED;
-    else // if(loopState == LOOP_STATE_STARTED)
-      loopState = LOOP_STATE_STOPPED;
-      digitalWrite(LED, LOW);
+  // nothing here!
+}
+void blinkLed() {
+  if (running)
+  {
+    digitalWrite(LED,HIGH);
+    running = false;
   }
-
-  if (loopState == LOOP_STATE_STARTED) {
-    digitalWrite(LED, HIGH);
+  else 
+  {
+    digitalWrite(LED, LOW);
+    running = true;
   }
 }
