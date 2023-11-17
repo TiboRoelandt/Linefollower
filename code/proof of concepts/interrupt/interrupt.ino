f43e530d41a8af280b2752da61be045a5c4da651
TiboRoelandt <146442904+TiboRoelandt@users.noreply.github.com>
2023-11-17 13:57:02 +0100

    interrupt.ino

--- code/proof of concepts/interrupt/interrupt.ino
+++ code/proof of concepts/interrupt/interrupt.ino
@@ -1,24 +1,47 @@
-const int LED = 3;
-const int BUTTON = 2;
-bool running;
+const byte ledPin = 3;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+// constants won't change. They're used here to set pin numbers:
+int ledState = HIGH;        // the current state of the output pin
+int buttonState;            // the current reading from the input pin
+int lastButtonState = LOW; 
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 200;    // the debounce time; increase if the output flickers
+
 void setup() {
-  pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT);
-  attachInterrupt(digitalPinToInterrupt(BUTTON), blinkLed, RISING);
-  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
+  
+  // set initial LED state
+  digitalWrite(ledPin, ledState);
 }
+
 void loop() {
-  // nothing here!
+  digitalWrite(ledPin, state);
+  int reading = digitalRead(interruptPin);
+
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
 }
-void blinkLed() {
-  if (running)
-  {
-    digitalWrite(LED,HIGH);
-    running = false;
-  }
-  else 
-  {
-    digitalWrite(LED, LOW);
-    running = true;
-  }
+
+void blink() {
+  state = !state;
 }
